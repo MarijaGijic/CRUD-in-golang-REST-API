932dec69af10dfe6320bacdc74271a853e45af30
Marija Gijic <45570836+MarijaGijic@users.noreply.github.com>
2022-10-07 20:24:55 +0200

    Create productcontroller.go

---
+++ controllers/productcontroller.go
@@ -0,0 +1,74 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-crud2/database"
+	"go-crud2/entities"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var product entities.Product             //new product variable
+	json.NewDecoder(r.Body).Decode(&product) //decodes the body of the infoming JSON request and maps it to the product
+	database.Instance.Create(&product)       //new table
+	json.NewEncoder(w).Encode(product)       //returns the newly created product data back to the client
+}
+
+func checkIfProductExists(productId string) bool {
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	if product.ID == 0 {
+		return false
+	}
+	return true
+}
+
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"] //gets the product id from the query string of the request
+	if checkIfProductExists(productId) == false {
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	var products []entities.Product   //new empty list of products
+	database.Instance.Find(&products) //maps all the available products into the product list var
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	productId := mux.Vars(r)["id"]
+	if checkIfProductExists(productId) == false {
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.First(&product, productId)
+	json.NewDecoder(r.Body).Decode(&product)
+	database.Instance.Save(&product)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	productId := mux.Vars(r)["id"]
+	if checkIfProductExists(productId) == false {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Product Not Found!")
+		return
+	}
+	var product entities.Product
+	database.Instance.Delete(&product, productId)
+	json.NewEncoder(w).Encode("Product Deleted Successfully!")
+}
